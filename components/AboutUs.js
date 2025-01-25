24562246a97e0acab212f290914fdc872af1d825
likithjoga12 <34797703+likithjoga12@users.noreply.github.com>
2025-01-25 19:03:58 +0530

    Add files via upload

---
+++ components/AboutUs.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css'
+import { NavLink } from 'react-router-dom'
+export const AboutUs = () => {
+  return (
+    <div>
+      <section>
+        <div className='loot'>
+        <div className="container"> 
+           <div className="row">
+            <div className="col-md-12 py-4"> 
+               <div className="text-center">
+                    <h2>About Us</h2>
+                     <ul className="breadcrumb" > 
+                      <li className="breadcrumb-item"><NavLink to="/">Home</NavLink></li>
+                      <li className="breadcrumb-item active">About Us</li>
+                      
+                        </ul> 
+                       </div> 
+                       </div>
+                       </div>
+                       </div>
+                       </div>
+
+   </section>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <h1 className="text-center">About Us</h1>
+            <p><strong>What is the purpose of this website?</strong>
+            This website was created to help people find a job that they are interested in. It will allow users to search for jobs based on their skills and interest
+            <br />This site was created to help people find information about their local area. It will also allow users to post reviews  
+            This site was created to help people who are interested in learning more about their favorite sports teams. This includes information such as team 
+            </p>
+            <p><strong>What is the purpose of this website?</strong>
+            This website was created to help people find a job that they are interested in. It will allow users to search for jobs based on their skills and interest
+            <br />This site was created to help people find information about their local area. It will also allow users to post reviews  
+            This site was created to help people who are interested in learning more about their favorite sports teams. This includes information such as team 
+            </p>
+            <p><strong>What is the purpose of this website?</strong>
+            This website was created to help people find a job that they are interested in. It will allow users to search for jobs based on their skills and interest
+            <br />This site was created to help people find information about their local area. It will also allow users to post reviews  
+            This site was created to help people who are interested in learning more about their favorite sports teams. This includes information such as team 
+            </p>
+            <h4>vision</h4>
+            <p><strong>What is the purpose of this website?</strong>
+            This website was created to help people find a job that they are interested in. It will allow users to search for jobs based on their skills and interest
+            <br />This site was created to help people find information about their local area. It will also allow users to post reviews  
+            This site was created to help people who are interested in learning more about their favorite sports teams. This includes information such as team 
+            </p>
+            <p><strong>What is the purpose of this website?</strong>
+            This website was created to help people find a job that they are interested in. It will allow users to search for jobs based on their skills and interest
+            <br />This site was created to help people find information about their local area. It will also allow users to post reviews  
+            This site was created to help people who are interested in learning more about their favorite sports teams. This includes information such as team 
+            </p>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
